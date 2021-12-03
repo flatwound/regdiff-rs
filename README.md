9b06ca210655e71605fd8e726c05a8c78a683ca6
flatwound <henkeshofs@gmail.com>
2021-12-03 13:44:06 +0100

    added diff functionality, started refactoring code into functions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Regdiff CLI tool
 Small cli tool to get a diff of registry after making a change, usefull for capturing settings from policies etc.
-
+Currently only supports REG_DWORD and REG_SZ values and the HKEY_LOCAL_MACHINE and HKEY_CURRENT_USER hives
 
 ## Usage:
     regdiff --hkcu
@@ -10,7 +10,6 @@ Small cli tool to get a diff of registry after making a change, usefull for capt
     regdiff --hkcu --subkey Console
     limits capturing regvalues from HKCU\Console only
 ## TODO:
-- Storing snapshot in vector/struct
-- Diff functionality
-- Refactor and make a tolerable code_base
-- Lot's of other stuff
+- Add other keys such as HKU, HKCR, HKCC
+- Add support for other registry types
+- Code review
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use clap::{App, Arg};
 use core::ptr::null_mut;
 use std::os::windows::prelude::OsStrExt;
 use windows::Win32::Foundation;
-use windows::Win32::System::Registry;
+use windows::Win32::System::Registry::{self, HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE};
 fn main() {
     let app = App::new("regdiff_cli")
         .version("0.1.0")
         .author("Henk Hofs <henkeshofs@gmail.com>")
-        .about("Quickly see what registry values have changed.")
+        .about("Quickly see what registry values have changed. Defaults to scanning HCU when no arguments supplied")
         .arg(
             Arg::with_name("subkey")
                 .short("s")
@@ -30,6 +30,10 @@ fn main() {
                 .help("Include HKCU results"),
         );
     let matches = app.clone().get_matches();
+    let mut hklm = false;
+    let mut hkcu = true;
+    let mut ret_val = false;
+
     if matches.is_present("hklm") && matches.is_present("hkcu") && matches.is_present("subkey") {
         println!("You can only use subkey with either hklm or hkcu, not both");
         let mut out = std::io::stdout();
@@ -37,52 +41,162 @@ fn main() {
         return;
     }
     let mut key_roots: Vec<Registry::HKEY> = vec![];
-    let mut key_path = String::new();
+
     if matches.is_present("hklm") {
-        println!("HKLM is turned on");
-        key_roots.push(Registry::HKEY_LOCAL_MACHINE);
+        hklm = true;
+        hkcu = false;
     }
     if matches.is_present("hkcu") {
-        println!("HKCU is turned on");
+        hkcu = true;
+    }
+    if hklm {
+        println!("Scanning HKLM is enabled");
+        key_roots.push(Registry::HKEY_LOCAL_MACHINE);
+    } else {
+        println!("Scanning HKLM is disabled")
+    }
+    if hkcu {
+        println!("Scanning HKCU is enabled");
         key_roots.push(Registry::HKEY_CURRENT_USER);
+    } else {
+        println!("Scanning HKCU is disabled")
     }
-
-    for key_root in key_roots {
+    let mut pre_map: std::collections::HashMap<String, RegData> = std::collections::HashMap::new();
+    for key_root in &key_roots {
+        let mut key_path = String::new();
+        let key_root_name = get_key_root_name(key_root);
+        key_path.push_str(key_root_name);
         if matches.is_present("subkey") {
-            println!("Processing {:?}", key_root);
-            enumerate_subkeys(
-                key_root,
-                matches.value_of("subkey").expect("mehmeh"),
-                &mut key_path,
-            )
+            let subkey_name = matches
+                .value_of("subkey")
+                .expect("Failed to get value of subkey, did you provide a valid string?");
+            println!("Processing {}\\{}", key_root_name, subkey_name);
+            ret_val = enumerate_subkeys(*key_root, subkey_name, &mut key_path, &mut pre_map)
         } else {
-            enumerate_subkeys(key_root, "", &mut key_path);
+            println!("Processing {}", key_root_name);
+            ret_val = enumerate_subkeys(*key_root, "", &mut key_path, &mut pre_map);
+        }
+    }
+    if ret_val == true {
+        println!("Snapshot saved, please make the modifications and press ENTER...");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let mut post_map: std::collections::HashMap<String, RegData> =
+            std::collections::HashMap::new();
+        for key_root in &key_roots {
+            let mut key_path = String::new();
+            let key_root_name = get_key_root_name(key_root);
+            key_path.push_str(key_root_name);
+            if matches.is_present("subkey") {
+                println!("Processing {:?}", key_root);
+                ret_val = enumerate_subkeys(
+                    *key_root,
+                    matches
+                        .value_of("subkey")
+                        .expect("Failed to get value of subkey, did you provide a valid string?"),
+                    &mut key_path,
+                    &mut post_map,
+                )
+            } else {
+                ret_val = enumerate_subkeys(*key_root, "", &mut key_path, &mut post_map);
+            }
+        }
+        if ret_val == true {
+            let mut result_added: std::collections::HashMap<String, RegData> =
+                std::collections::HashMap::new();
+            let mut result_removed: std::collections::HashMap<String, RegData> =
+                std::collections::HashMap::new();
+            let mut result_changed: std::collections::HashMap<String, RegData> =
+                std::collections::HashMap::new();
+            for (k, v) in pre_map.iter() {
+                if post_map.contains_key(k) {
+                    if post_map.get(k).unwrap() != pre_map.get(k).unwrap() {
+                        result_changed.insert(k.clone(), post_map.get(k).unwrap().clone());
+                    }
+                } else {
+                    result_removed.insert(k.clone(), v.clone());
+                }
+            }
+            for (k, v) in post_map.iter() {
+                if !pre_map.contains_key(k) {
+                    result_added.insert(k.clone(), v.clone());
+                }
+            }
+            println!("===================");
+            println!("Values added:");
+            println!("===================");
+            print_map(&result_added, false);
+            println!("===================");
+            println!("Values removed:");
+            println!("===================");
+            print_map(&result_removed, true);
+            println!("===================");
+            println!("Values changed:");
+            println!("===================");
+            print_map(&result_changed, false);
         }
     }
 }
-struct RegistryItem {
-    key: String,
-    value_type: u32,
-    value_name: String,
-    data: RegData,
-}
-enum RegData {
-    REG_DWORD(u32),
-    REG_SZ(String),
+fn get_key_root_name(key_root: &Registry::HKEY) -> &str {
+    match *key_root {
+        HKEY_CURRENT_USER => "HKEY_CURRENT_USER",
+        HKEY_LOCAL_MACHINE => "HKEY_LOCAL_MACHINE",
+        _ => "",
+    }
 }
 
-fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mut String) {
-    let mut key = Registry::HKEY::default();
-    if key_path.len() == 0 {
-        match h_key {
-            Registry::HKEY_CURRENT_USER => key_path.push_str("HKCU\\"),
-            Registry::HKEY_LOCAL_MACHINE => key_path.push_str("HKLM\\"),
-            _ => {}
+#[derive(PartialEq, Eq, Debug, Clone)]
+enum RegData {
+    RegDword(u32),
+    RegSz(String),
+}
+fn print_map(map: &std::collections::HashMap<String, RegData>, removed: bool) {
+    for (k, v) in map {
+        let key = k.split(';').nth(0).unwrap();
+        let value = k.split(';').nth(1).unwrap();
+        match v {
+            RegData::RegDword(d) => {
+                println!("[{}]", key);
+                if removed == true {
+                    println!("-\"{}\"=dword:{}", value, d);
+                } else {
+                    println!("\"{}\"=dword:{}", value, d);
+                }
+            }
+            RegData::RegSz(s) => {
+                println!("[{}]", key);
+                if removed == true {
+                    println!("-\"{}\"=\"{}\"", value, s);
+                } else {
+                    println!("\"{}\"=\"{}\"", value, s);
+                }
+            }
         }
     }
+}
+
+fn enumerate_subkeys(
+    h_key: Registry::HKEY,
+    subkey_path: &str,
+    mut key_path: &mut String,
+    mut map: &mut std::collections::HashMap<String, RegData>,
+) -> bool {
+    let mut ret_val = true;
+    let mut key = Registry::HKEY::default();
+    // if key_path.len() == 0 {
+    //     match h_key {
+    //         Registry::HKEY_CURRENT_USER => key_path.push_str("HKCU\\"),
+    //         Registry::HKEY_LOCAL_MACHINE => key_path.push_str("HKLM\\"),
+    //         _ => {}
+    //     }
+    // }
     if subkey_path.len() > 0 {
-        key_path.push_str(subkey_path);
         key_path.push_str("\\");
+        key_path.push_str(subkey_path);
+        //key_path.push_str("\\");
     }
 
     let mut sk = encode_wide(subkey_path);
@@ -94,8 +208,11 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
 
         if status.0 != Foundation::ERROR_SUCCESS.0 as i32 {
             if status.0 == Foundation::ERROR_ACCESS_DENIED.0 as i32 {
-                println!("ACCESS_DENIED on {}", subkey_path);
-                return;
+                //println!("ACCESS_DENIED on {}", subkey_path);
+                return false;
+            } else if status.0 == Foundation::ERROR_FILE_NOT_FOUND.0 as i32 {
+                println!("Could not find subkey: {}", subkey_path);
+                return false;
             } else {
                 panic!(
                     "Could not open h_key {:?} with subkey {}: status: {:?}",
@@ -127,12 +244,17 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
         )
     };
     if status.0 != Foundation::ERROR_SUCCESS.0 as i32 {
-        panic!("Could not query h_key at {}", subkey_path);
+        if status.0 == Foundation::ERROR_ACCESS_DENIED.0 as i32 {
+            //println!("ACCESS_DENIED on {}", subkey_path);
+            return false;
+        } else {
+            panic!("Could not query h_key at {}: {:?}", subkey_path, status);
+        }
     }
-    println!(
-        "I found {} subkeys and {} values in {}",
-        subkey_count, value_count, subkey_path
-    );
+    // println!(
+    //     "I found {} subkeys and {} values in {}",
+    //     subkey_count, value_count, subkey_path
+    // );
     for i in 0..subkey_count {
         let mut subkey_len = max_subkey_len + 1;
         let mut buffer: Vec<u16> = vec![0; subkey_len as usize];
@@ -149,21 +271,53 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
             )
         };
         if status.0 != Foundation::ERROR_SUCCESS.0 as i32 {
-            panic!("Could not enum subkeys at {}", subkey_path);
+            if status.0 == Foundation::ERROR_ACCESS_DENIED.0 as i32 {
+                //println!("ACCESS_DENIED on {}", subkey_path);
+                return false;
+            } else {
+                panic!("Could not enum subkeys at {}: {:?}", subkey_path, status);
+            }
         }
         let subkey_name =
             String::from_utf16(&buffer[..subkey_len as usize]).expect("Could not get subkey name");
         //println!("Found subkey {} {}", subkey_name, key_path);
 
-        enumerate_subkeys(key, subkey_name.as_str(), &mut key_path);
-        for c in subkey_name.chars() {
+        ret_val = enumerate_subkeys(key, subkey_name.as_str(), &mut key_path, &mut map);
+        // Remove current keyname from the key_path string, since we're going back to parent
+        for _ in subkey_name.chars() {
             key_path.pop();
         }
+        // Additional pop for the \ character
         key_path.pop();
     }
-    //key_path.pop();
-    //
 
+    ret_val = enumerate_values(
+        &key,
+        value_count,
+        max_value_name_len,
+        max_data_len,
+        &key_path,
+        &mut map,
+    );
+    ret_val
+}
+
+fn encode_wide(input: &str) -> Vec<u16> {
+    let vec: Vec<u16> = std::ffi::OsStr::new(input)
+        .encode_wide()
+        .chain(std::iter::once(0))
+        .collect();
+    vec
+}
+
+fn enumerate_values(
+    key: &Registry::HKEY,
+    value_count: u32,
+    max_value_name_len: u32,
+    max_data_len: u32,
+    key_path: &String,
+    map: &mut std::collections::HashMap<String, RegData>,
+) -> bool {
     for i in 0..value_count {
         let mut value_len = max_value_name_len + 1;
         let mut value_type: u32 = 0;
@@ -173,7 +327,7 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
 
         let status = unsafe {
             Registry::RegEnumValueW(
-                &key,
+                key,
                 i,
                 Foundation::PWSTR(buffer.as_mut_ptr()),
                 &mut value_len,
@@ -184,7 +338,12 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
             )
         };
         if status.0 != Foundation::ERROR_SUCCESS.0 as i32 {
-            panic!("Could not enum values in subkey");
+            if status.0 == Foundation::ERROR_ACCESS_DENIED.0 as i32 {
+                //println!("ACCESS_DENIED on {}", subkey_path);
+                return false;
+            } else {
+                panic!("Could not enum values in subkey {}: {:?}", key_path, status);
+            }
         }
         let reg_type = Registry::REG_VALUE_TYPE { 0: value_type };
         let mut val_name = String::from_utf16(&buffer[..value_len as usize])
@@ -193,16 +352,16 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
         if val_name.len() == 0 {
             val_name = String::from("(Default)");
         }
-        println!("[{}]{} of type {}", key_path, val_name, value_type);
+        //println!("[{}]{} of type {}", key_path, val_name, value_type);
         match reg_type {
             Registry::REG_DWORD => {
                 //let mut dw_data: Vec<u32> = vec![0; data_len as usize];
                 let mut dw_data = 0u32;
                 let dw_void: *const u32 = &dw_data as *const u32;
                 let mut dw_size = data_len; //std::mem::size_of::<u32>() as u32;
-                let mut status = unsafe {
+                let status = unsafe {
                     Registry::RegGetValueW(
-                        &key,
+                        key,
                         Foundation::PWSTR::default(),
                         Foundation::PWSTR(buffer.as_mut_ptr()),
                         Registry::RRF_RT_REG_DWORD,
@@ -215,14 +374,17 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
                 if status.0 != Foundation::ERROR_SUCCESS.0 as i32 {
                     panic!("Could not get value {} in subkey {:?}", val_name, status);
                 }
-                let dword = RegData::REG_DWORD(dw_data);
-                println!("data {:#08x}", dw_data);
+                let dword = RegData::RegDword(dw_data);
+
+                map.insert(format!("{};{}", key_path, val_name), dword);
+
+                //println!("data {:#08x}", dw_data);
             }
             Registry::REG_SZ => {
                 let mut data_size: u32 = data_len;
                 let status = unsafe {
                     Registry::RegGetValueW(
-                        &key,
+                        key,
                         Foundation::PWSTR::default(),
                         Foundation::PWSTR(buffer.as_mut_ptr()),
                         Registry::RRF_RT_REG_SZ,
@@ -241,7 +403,7 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
 
                 let status = unsafe {
                     Registry::RegGetValueW(
-                        &key,
+                        key,
                         Foundation::PWSTR::default(),
                         Foundation::PWSTR(buffer.as_mut_ptr()),
                         Registry::RRF_RT_REG_SZ,
@@ -257,17 +419,13 @@ fn enumerate_subkeys(h_key: Registry::HKEY, subkey_path: &str, mut key_path: &mu
                 let val_data = String::from_utf16(&buffer2[..new_datasize as usize])
                     .expect("Could not read reg_sz string from buffer");
                 //let val_data = buffer2;
-                println!("data {}", val_data);
+                let reg_sz = RegData::RegSz(val_data.clone());
+                map.insert(format!("{};{}", key_path, val_name), reg_sz);
             }
-            _ => {}
+            _ => {
+                return false;
+            }
         }
     }
-}
-
-fn encode_wide(input: &str) -> Vec<u16> {
-    let vec: Vec<u16> = std::ffi::OsStr::new(input)
-        .encode_wide()
-        .chain(std::iter::once(0))
-        .collect();
-    vec
+    true
 }
